d8b770c57fced073a75518ea75758148be404322
James Winchester <wildrover254@gmail.com>
2022-02-21 12:34:59 -0500

    Added Cookie support

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
+import Cookies from 'js-cookie';
 import Data from "./Data";
 
 export const Context = React.createContext();
 
 export class Provider extends Component {
-
-    state = {
-        authenticatedUser: null
-    }
-
     constructor() {
         super();
         this.data = new Data();
+        this.cookie = Cookies.get('authenticatedUser');
+        this.state = {
+            authenticatedUser: this.cookie ? JSON.parse(this.cookie) : null
+        };
     }
 
     render() {
@@ -41,12 +41,14 @@ export class Provider extends Component {
                     authenticatedUser: user,
                 };
             });
+            Cookies.set('authenticatedUser', JSON.stringify(user), { expires: 1 });
         }
         return user;
     }
 
     signOut = () => {
         this.setState({ authenticatedUser: null });
+        Cookies.remove('authenticatedUser');
     }
 }
 
